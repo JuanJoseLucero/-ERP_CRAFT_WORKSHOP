e244869cac27a4f0b5a2755d2a918cc5d0b1b004
= <juanjolucero@hotmail.es>
2025-07-22 23:43:43 -0500

    ADD: FIX BUGS

--- src/main/java/com/cjconfecciones/controller/ListOrderController.java
+++ src/main/java/com/cjconfecciones/controller/ListOrderController.java
@@ -189,8 +189,9 @@ public class ListOrderController implements Serializable{
 
 	public void closeWindowsAbono(){
 		try{
-			orders = apiRestClient.consumeWebServices(ListOrder.class, "order/getOrders", "");
-			PrimeFaces.current().ajax().update("billForm:ordersIdTable");
+//			orders = apiRestClient.consumeWebServices(ListOrder.class, "order/getOrders", "");
+//			PrimeFaces.current().ajax().update("billForm:ordersIdTable");
+			this.filtrar();
 		}catch (Exception e){
 			log.log(Level.SEVERE, "ERROR TO MOPDIFY ORDER ",e);
 		}
